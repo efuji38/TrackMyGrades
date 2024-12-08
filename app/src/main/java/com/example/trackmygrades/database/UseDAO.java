e319623f464143ef5ad4bc334aca2c19cf351f43
efuji38 <efujiwara@csumb.edu>
2024-12-08 15:51:27 -0800

    Added some fields and methods in User.java and UserDAO

--- app/src/main/java/com/example/trackmygrades/database/UseDAO.java
+++ app/src/main/java/com/example/trackmygrades/database/UseDAO.java
@@ -1,4 +1,20 @@
 package com.example.trackmygrades.database;
 
-public interface UseDAO {
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.trackmygrades.database.entities.User;
+
+@Dao
+public interface UserDAO {
+    @Insert
+    void insert(User... user);
+    @Query("DELETE FROM " + TrackMyGradesDatabase.USER_TABLE)
+    void deleteALL();
+
+    @Query("SELECT * FROM " + TrackMyGradesDatabase.USER_TABLE + " WHERE username == :username")
+    LiveData<User> getUserByUserName(String username);
 }
+
--- app/src/main/java/com/example/trackmygrades/database/entities/User.java
+++ app/src/main/java/com/example/trackmygrades/database/entities/User.java
@@ -1,4 +1,83 @@
 package com.example.trackmygrades.database.entities;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Objects;
+
+@Entity(tableName = "userTable")
 public class User {
+
+    @PrimaryKey(autoGenerate = true)
+    private int userId;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String username;
+    private String email;
+    private String password;
+    private boolean isTeacher;
+
+    public User(String username, String email, boolean isTeacher, String password, int userId) {
+        this.username = username;
+        this.email = email;
+        this.isTeacher = isTeacher;
+        this.password = password;
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return userId == user.userId && isTeacher == user.isTeacher && Objects.equals(email, user.email) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, email, password, isTeacher);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isTeacher() {
+        return isTeacher;
+    }
+
+    public void setTeacher(boolean teacher) {
+        isTeacher = teacher;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
 }
+
+
+
