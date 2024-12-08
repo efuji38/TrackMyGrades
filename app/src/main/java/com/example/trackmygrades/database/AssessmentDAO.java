4a727c759eb118b001e2efd90bf5b22c05d43182
efuji38 <efujiwara@csumb.edu>
2024-12-08 15:48:00 -0800

    Added some fields and methods in Assessment.java and AssessmentDAO

--- app/src/main/java/com/example/trackmygrades/database/AssessmentDAO.java
+++ app/src/main/java/com/example/trackmygrades/database/AssessmentDAO.java
@@ -1,4 +1,16 @@
 package com.example.trackmygrades.database;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.trackmygrades.database.entities.Assessment;
+@Dao
 public interface AssessmentDAO {
+
+    @Insert
+    void insert(Assessment... assessment);
+    @Query("DELETE FROM " + TrackMyGradesDatabase.ASSESSMENT_TABLE)
+    void deleteALL();
 }
+
--- app/src/main/java/com/example/trackmygrades/database/entities/Assessment.java
+++ app/src/main/java/com/example/trackmygrades/database/entities/Assessment.java
@@ -1,4 +1,68 @@
 package com.example.trackmygrades.database.entities;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity(tableName = "assessmentTable")
 public class Assessment {
+    @PrimaryKey(autoGenerate = true)
+    private int assignmentId;
+    private String title;
+    private LocalDateTime dueDate;
+    private int teacherId;
+
+    public Assessment(int assignmentId, LocalDateTime dueDate, int teacherId, String title) {
+        this.assignmentId = assignmentId;
+        this.dueDate = dueDate;
+        this.teacherId = teacherId;
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Assessment that = (Assessment) o;
+        return assignmentId == that.assignmentId && teacherId == that.teacherId && Objects.equals(title, that.title) && Objects.equals(dueDate, that.dueDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(assignmentId, title, dueDate, teacherId);
+    }
+
+    public int getAssignmentId() {
+        return assignmentId;
+    }
+
+    public void setAssignmentId(int assignmentId) {
+        this.assignmentId = assignmentId;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDateTime dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
